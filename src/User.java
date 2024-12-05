406d61f2982db96482d917f0d00b38f4826b6d38
Elsa Ryrlen <elsa.ryrlen.1533@student.lu.se>
2024-12-05 12:29:01 +0100

    changes ≈

--- src/User.java
+++ src/User.java
@@ -6,21 +6,30 @@ import java.util.*;
  * 
  */
 public class User {
-
+    private String userName;
+    private String password;
     /**
      * Default constructor
      */
-    public User() {
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPassword() {
+        return password;
     }
 
     /**
      * 
      */
-    private String userName;
 
     /**
      * 
      */
-    private String password;
 
 }
